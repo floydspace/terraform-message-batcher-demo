e52e69492bd9bc7ea87e94232bffba438270c006
Victor Korzunin <victor.korzunin@blanco.services>
2021-06-07 00:16:53 +0200

    step 2

--- main.tf
+++ main.tf
@@ -11,3 +11,32 @@ provider "aws" {
   profile = "default"
   region  = var.region
 }
+
+resource "aws_dynamodb_table" "default" {
+  name             = "${local.service_name}-table-${var.stage}"
+  hash_key         = "criteria"
+  billing_mode     = "PAY_PER_REQUEST"
+  stream_enabled   = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
+  tags             = local.aws_default_tags
+
+  attribute {
+    name = "criteria"
+    type = "S"
+  }
+}
+
+resource "aws_sqs_queue" "default_queue" {
+  name = "${local.service_name}-queue-${var.stage}"
+  tags = local.aws_default_tags
+
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.deadletter_queue.arn
+    maxReceiveCount     = 2
+  })
+}
+
+resource "aws_sqs_queue" "deadletter_queue" {
+  name = "${local.service_name}-dlq-${var.stage}"
+  tags = local.aws_default_tags
+}
