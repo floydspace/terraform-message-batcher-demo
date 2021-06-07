043fd41da1f5c013ffc87c1980b0e0f237ceda4a
Victor Korzunin <victor.korzunin@blanco.services>
2021-06-07 21:03:20 +0200

    step 4

---
+++ lambdas/dynamodb_stream/app.py
@@ -0,0 +1,34 @@
+import boto3
+import os
+import json
+
+
+def handler(event, context):
+    queue_arn = os.environ['QUEUE_ARN']
+    event_name = event['Records'][0]['eventName']
+    print(event)
+
+    if event_name == 'INSERT':
+        boto3.resource('dynamodb')
+        sqs = boto3.resource('sqs')
+
+        deserializer = boto3.dynamodb.types.TypeDeserializer()
+        keys = {
+            k: deserializer.deserialize(v) for k, v in event['Records'][0]['dynamodb']['Keys'].items()
+        }
+
+        queue = sqs.get_queue_by_name(
+            QueueName=queue_arn.split(':')[-1]
+        )
+
+        response = queue.send_message(
+            MessageBody=json.dumps(keys, ensure_ascii=False),
+            DelaySeconds=30,
+        )
+
+        print(response)
+
+        return
+
+    if event_name == 'REMOVE':
+        print(event)
--- main.tf
+++ main.tf
@@ -111,3 +111,71 @@ resource "aws_lambda_event_source_mapping" "queue_message" {
   batch_size       = 1
   depends_on       = [aws_iam_role.queue_message]
 }
+
+resource "aws_lambda_function" "dynamodb_stream" {
+  function_name    = "${local.service_name}-dynamodb-stream-${var.stage}"
+  role             = aws_iam_role.dynamodb_stream.arn
+  filename         = data.archive_file.dynamodb_stream.output_path
+  source_code_hash = data.archive_file.dynamodb_stream.output_base64sha256
+  handler          = "app.handler"
+  runtime          = "python3.8"
+  tags             = local.aws_default_tags
+
+  environment {
+    variables = {
+      QUEUE_ARN = aws_sqs_queue.default_queue.arn
+    }
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.dynamodb_stream,
+    aws_iam_role_policy_attachment.sqs_access,
+    aws_cloudwatch_log_group.dynamodb_stream,
+  ]
+}
+
+data "archive_file" "dynamodb_stream" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambdas/dynamodb_stream"
+  output_path = "${path.cwd}/.deployment/dynamodb_stream.zip"
+}
+
+resource "aws_iam_role" "dynamodb_stream" {
+  name = "${local.service_name}-dynamodb-stream-${var.stage}-lambdaRole"
+
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+
+  inline_policy {
+    name   = "${local.service_name}-dynamodb-stream-${var.stage}-lambdaPolicy"
+    policy = data.aws_iam_policy_document.dynamodb_stream.json
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "dynamodb_stream" {
+  role       = aws_iam_role.dynamodb_stream.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaDynamoDBExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "sqs_access" {
+  role       = aws_iam_role.dynamodb_stream.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSQSReadOnlyAccess"
+}
+
+resource "aws_cloudwatch_log_group" "dynamodb_stream" {
+  name = "/aws/lambda/${local.service_name}-dynamodb-stream-${var.stage}"
+}
+
+data "aws_iam_policy_document" "dynamodb_stream" {
+  statement {
+    actions   = ["sqs:SendMessage"]
+    resources = [aws_sqs_queue.default_queue.arn]
+  }
+}
+
+resource "aws_lambda_event_source_mapping" "dynamodb_stream" {
+  event_source_arn  = aws_dynamodb_table.default.stream_arn
+  function_name     = aws_lambda_function.dynamodb_stream.arn
+  starting_position = "LATEST"
+  batch_size        = 1
+  depends_on        = [aws_iam_role.dynamodb_stream]
+}
